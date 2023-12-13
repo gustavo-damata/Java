ff5dc5cfa5ce00a78a5a554f5979f47ea7ab3b53
gustavo-damata <150547601+gustavo-damata@users.noreply.github.com>
2023-12-13 12:35:50 -0300

    Add files via upload

---
+++ public class Main {.java
@@ -0,0 +1,20 @@
+
+/*Criando um Reprodutor Musical*/
+/*Criando funções no Aparelho Telefonico*/
+/*Criando Navegador Internet*/
+
+Public class Telefonico implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+    + tocar() { // implementação }
+    + pausar() { // implementação }
+    + selecionarMusica() { // implementação }
+  
+    + ligar() { // implementação }
+    + atender() { // implementação }
+    + iniciarCorreioVoz() { // implementação }
+  
+    + exibirPagina() { // implementação }
+    + adicionarNovaAba() { // implementação }
+    + atualizarPagina() { // implementação }
+  }
+
+
